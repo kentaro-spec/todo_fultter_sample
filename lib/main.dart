2456924706ae2d4c961d0ffda1bd2270eab26d98
kentaro-spec <tsalm10@yahoo.co.jp>
2020-11-30 20:11:10 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'main_model.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TODOアプリ',
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel()..getTodoList(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('TODOアプリ'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model, child) {
+            final todoList = model.todoList;
+            return ListView(
+              children: todoList
+                  .map(
+                    (todo) => ListTile(
+                      title: Text(todo.title),
+                    ),
+                  )
+                  .toList(),
+            );
+          }),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Increment',
+            child: Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
+      ),
+    );
+  }
+}
